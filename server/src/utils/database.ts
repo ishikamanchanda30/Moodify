ff19a1a300987a6798aa5caac00582a5c43cd3d5
ishikamanchanda30 <ishikamanchanda0130@gmail.com>
2025-05-23 16:44:38 +0530

    database connect

---
+++ server/src/utils/database.ts
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const mongourl = process.env.MONGOURL;
+
+if (!mongourl) {
+  throw new Error('MONGOURL environment variable is not defined');
+}
+
+export const connectDB = async() : Promise <void> => {
+    try {
+        await mongoose.connect(mongourl);
+        console.log("Database Connected!");
+    } catch (e) {
+        console.log("Error in connecting to DATABASE", e);
+        process.exit(1);
+    }
+}
